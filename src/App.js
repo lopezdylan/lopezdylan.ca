9bdfe479a24fdcd42d0a04ad32aab340e4b4cf8f
Dylan Lopez <dylan.lopez94@gmail.com>
2021-09-21 20:25:58 -0400

    Adding react build after prevous commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coming Soon
+          Portfolio coming soon
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          GitHub
+          My GitHub
         </a>
       </header>
     </div>
