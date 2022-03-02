60b7f35521bba6fd553b0ccd007a79a92d7ad132
Dylan Lopez <dylan.lopez94@gmail.com>
2022-03-01 21:04:53 -0500

    Changing temp landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Portfolio coming soon
+          Portfolio coming soon!!!
         </p>
         <a
           className="App-link"
