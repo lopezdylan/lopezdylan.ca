f2aa955e35868625eed169c2fe1d1c81a62b0abf
Dylan Lopez <dylan.lopez94@gmail.com>
2022-03-12 20:03:41 -0500

    Reinitialize sanity backend with schemas

--- portfolio_studio/schemas/schema.js
+++ portfolio_studio/schemas/schema.js
@@ -10,18 +10,14 @@ import skills from './skills'
 import workExperience from './workExperience'
 import contact from './contact'
 
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    works, 
-    abouts, 
-    skills, 
-    workExperience, 
-    experiences, 
-    contact
+  types: schemaTypes.concat([works, abouts, skills, workExperience, experiences, contact
+    /* Your types here! */
   ]),
 })
