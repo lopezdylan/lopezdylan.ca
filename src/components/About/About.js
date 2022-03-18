3c62cf67a4d77fb83a908331f33012f99bdd30b6
Dylan Lopez <dylan.lopez94@gmail.com>
2022-03-18 11:06:19 -0400

    Fixed centering in mobile

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,40 +17,40 @@ const About = () => {
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
       <p className='about__desc'>{closingDescription && closingDescription}</p>
-      <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-      </div>
-      <div className='about__link__margins'>
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <SiGithub />
-              </a>
-            )}
+        <div className='about__contact center'>
+          {resume && (
+            <a href={resume}>
+              <span type='button' className='btn btn--outline'>
+                Resume
+              </span>
+            </a>
+          )}
+        </div>
+        <div className='about__link__margins'>
+          {social && (
+            <>
+              {social.github && (
+                <a
+                  href={social.github}
+                  aria-label='github'
+                  className='link link--icon'
+                >
+                  <SiGithub />
+                </a>
+              )}
 
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <SiLinkedin />
-              </a>
-            )}
-          </>
-        )}
-      </div>
+              {social.linkedin && (
+                <a
+                  href={social.linkedin}
+                  aria-label='linkedin'
+                  className='link link--icon'
+                >
+                  <SiLinkedin />
+                </a>
+              )}
+            </>
+          )}
+        </div>
     </div>
   )
 }
